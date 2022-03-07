4905b9e5b88a4105392d05d6981fec93aa5abd5c
jollaman999 <admin@jollaman999.com>
2022-03-07 23:08:53 +0900

    ARN 발생 - 시간이 오래걸리는 작업

--- intent_filter_test/src/main/java/com/jolla/intent_filter_test/MainActivity.kt
+++ intent_filter_test/src/main/java/com/jolla/intent_filter_test/MainActivity.kt
@@ -2,10 +2,25 @@ package com.jolla.intent_filter_test
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.jolla.intent_filter_test.databinding.ActivityMainBinding
+import kotlin.system.measureTimeMillis
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnClick.setOnClickListener {
+            var sum = 0L
+            val time = measureTimeMillis {
+                for (i in 1..9_000_000_000) {
+                    sum += i
+                }
+            }
+            Log.d("jolla", "time : $time")
+            binding.resultView.text = "sum : $sum"
+        }
     }
 }
