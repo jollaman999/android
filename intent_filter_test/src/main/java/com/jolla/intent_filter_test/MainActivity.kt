ac7705ce6546e709e41ef80d3779350af105fd8d
jollaman999 <admin@jollaman999.com>
2022-03-07 23:16:54 +0900

    ANR 발생 해결 - 스레드 핸들러 구조

--- intent_filter_test/src/main/java/com/jolla/intent_filter_test/MainActivity.kt
+++ intent_filter_test/src/main/java/com/jolla/intent_filter_test/MainActivity.kt
@@ -2,8 +2,11 @@ package com.jolla.intent_filter_test
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Message
 import android.util.Log
 import com.jolla.intent_filter_test.databinding.ActivityMainBinding
+import kotlin.concurrent.thread
 import kotlin.system.measureTimeMillis
 
 class MainActivity : AppCompatActivity() {
@@ -12,15 +15,26 @@ class MainActivity : AppCompatActivity() {
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btnClick.setOnClickListener {
-            var sum = 0L
-            val time = measureTimeMillis {
-                for (i in 1..9_000_000_000) {
-                    sum += i
-                }
+        val handler = object: Handler() {
+            override fun handleMessage(msg: Message) {
+                super.handleMessage(msg)
+                binding.resultView.text = "sum : ${msg.arg1}"
             }
-            Log.d("jolla", "time : $time")
-            binding.resultView.text = "sum : $sum"
+        }
+
+        binding.btnClick.setOnClickListener {
+            thread {
+                var sum = 0L
+                val time = measureTimeMillis {
+                    for (i in 1..9_000_000_000) {
+                        sum += i
+                    }
+                    val message = Message()
+                    message.arg1 = sum.toInt()
+                    handler.sendMessage(message)
+                }
+                Log.d("jolla", "time : $time")
+            }
         }
     }
 }
